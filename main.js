d0836f4259002e5403e78914af9b65ba22c29d9f
Crystian Oliveira <crystianmendonca2003@gmail.com>
2023-02-03 13:06:55 -0300

    Final da base do JS calculadora

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 //Selecionando os botões e o painel de visualização
 const btns = document.querySelectorAll("button");
-const resultField = document.querySelector("#resultado");
-
-//O sinal += é usado para adicionar um valor a uma variável já existente. Exemplo:
-//let num = 1;
-//num += 5;
-//console.log(num); // 6
-
-
-//Escutador se vai ser clicado algum botão
+let resultField = document.querySelector("#resultado");
 
 btns.forEach(btn => {
     btn.addEventListener('click', () => {
-        resultField.value += btn.value.textContent;
+        let num = btn.innerHTML
+        if (resultField.value == "0") {
+            resultField.value = num
+         } else if (num == "AC") {
+             resultField.value = 0
+          } else if (num == ",") {
+            resultField.value += "."
+          } else if (num == "=") {
+            resultField.value = eval(resultField.value);
+         } else {
+            resultField.value += num
+        }
 
-
-
-
+        console.log(num)
 
     });
 });
